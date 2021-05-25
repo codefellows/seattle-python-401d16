8e63183e4a845ff8846d103234091e52d4c93b50
JB-Tellez <jb.tellez@gmail.com>
2021-05-25 13:01:04 -0700

    organizes tests and adds starter game.py

---
+++ class-07/lab/game_of_greed/game.py
@@ -0,0 +1,28 @@
+import sys
+
+from game_of_greed.game_logic import GameLogic
+from game_of_greed.banker import Banker
+
+
+class Game:
+    """Class for Game of Greed application
+    """
+
+    def __init__(self, num_rounds=20):
+
+        self.banker = Banker()
+        self.num_rounds = num_rounds
+        self.round_num = 0
+        self.roller = None
+
+    def play(self, roller=None):
+        """Entry point for playing (or declining) a game
+
+        Args:
+            roller (function, optional): Allows passing in a custom dice roller function.
+                Defaults to None.
+        """
+
+        self.roller = roller or GameLogic.roll_dice
+
+        print("Welcome to Game of Greed")
