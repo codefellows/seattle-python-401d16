c9122c35a1fae412f07f976e68133797fd28179d
JB-Tellez <jb.tellez@gmail.com>
2021-06-21 12:02:57 -0700

    adds finalized demo

--- class-26/demo/django-things/things/urls.py
+++ class-26/demo/django-things/things/urls.py
@@ -2,6 +2,9 @@
 
 from django.urls import path
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
-urlpatterns = [path("", HomePageView.as_view(), name="home")]
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+]
